d6aa1205568553d490892fb139ed2e1709aea476
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-07-26 12:16:42 +0300

    game cells added

---
+++ cell.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "color.hpp"
+#include "setup.hpp"
+
+using namespace std;
+
+enum CELL_STATES
+{
+    EMPTY,
+    MINE,
+    ADJ_TO_MINE
+};
+
+COLOR ADJMINECOLOURS[] = {
+    reset,
+    cyan_fg,    //1
+    green_fg,   //2
+    red_fg,     //3
+    magenta_fg, //4
+    yellow_fg,  //5
+    white_fg,   //6
+    magenta_fg, //7
+    red_fg      //8
+};
+
+struct Cell
+{
+    Cell();
+    void toggleflag();
+    void reveal();
+    void setMine();
+    void markAdjMine(int);
+    bool flagged;
+    bool hidden;
+    CELL_STATES state;
+    int noOfAdjMines;
+    string sym;
+};
+
+Cell::Cell()
+{
+    hidden = true;
+    flagged = false;
+    state = EMPTY;
+    sym = white_bg + " ";
+}
+
+void Cell::toggleflag()
+{
+    if (hidden)
+    {
+        flagged = !flagged;
+        if (flagged)
+            sym = green_fg + "▶";
+        
+        else
+            sym = white_bg + " ";
+    }
+}
+
+void Cell::reveal()
+{
+    if (!flagged && hidden)
+    {
+        hidden = false;
+        switch (state)
+        {
+        case EMPTY:
+            sym = reset + " ";
+            return;
+        case MINE:
+            sym = red_fg + "✸";
+            return;
+        case ADJ_TO_MINE:
+            sym = ADJMINECOLOURS[noOfAdjMines] + to_string(noOfAdjMines);
+            return;
+        }
+    }
+}
+
+void Cell::markAdjMine(int mines)
+{
+    state = ADJ_TO_MINE;
+    noOfAdjMines = mines;
+}
+
+void Cell::setMine()
+{
+    state = MINE;
+}
+
+using GRID = vector<vector<Cell>>;
