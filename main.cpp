280521eee8e10a774ebd01987c4232bc013bc94f
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-07-26 13:00:06 +0300

    main loop added

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "field.hpp"
+#include "buffer.hpp"
+#include "setup.hpp"
+#include "color.hpp"
+#include "display.hpp"
+
+int main()
+{
+    system("clear");
+    setGameMode();
+    getQuickClearSettings();
+
+    Field field;
+
+    while (true)
+    {
+        system("clear");
+
+        dispBanner();
+        field.drawField();
+        dispFlagCounter();
+        
+        if (gameState != RUNNING)
+            dispVictoryOrDefeat();
+        else
+            dispControls();
+
+        writeBuf.disp();
+        writeBuf.clear();
+
+        if (gameState == RUNNING)
+            field.getMove();
+        else
+            break;
+    }
+
+    std::cout << endl << reset;
+    return 0;
+}
